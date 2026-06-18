6b691a09f186b9562f03b5ed052d51772431bc55
Claudio Guevara <claudio.guevara.dev@gmail.com>
2026-06-17 20:45:55 -0400

    fix(ui): remove object details sidebar

--- src/app/workspace/WorkspaceShell.tsx
+++ src/app/workspace/WorkspaceShell.tsx
@@ -1,9 +1,7 @@
-import { cn } from "../../lib/utils";
 import { DialogsHost } from "../components/dialogs/DialogsHost";
 import { Explorer } from "../components/explorer/Explorer";
 import { StatusBar } from "../components/layout/StatusBar";
 import { TopBar } from "../components/layout/TopBar";
-import { ObjectDetailsPanel } from "../components/object-details/ObjectDetailsPanel";
 import { MainWorkspace } from "../components/workspace/MainWorkspace";
 import { useWorkspaceLayout } from "./workspaceCore";
 
@@ -13,17 +11,9 @@ export function WorkspaceShell() {
   return (
     <div className="flex h-screen min-h-0 flex-col bg-background text-[13px] text-foreground">
       <TopBar onNewConnection={workspace.openNewConnectionDialog} />
-      <div
-        className={cn(
-          "grid min-h-0 flex-1",
-          workspace.requiresConnection
-            ? "grid-cols-[288px_minmax(560px,1fr)]"
-            : "grid-cols-[288px_minmax(560px,1fr)_336px]",
-        )}
-      >
+      <div className="grid min-h-0 flex-1 grid-cols-[288px_minmax(560px,1fr)]">
         <Explorer />
         <MainWorkspace requiresConnection={workspace.requiresConnection} />
-        {workspace.requiresConnection ? null : <ObjectDetailsPanel />}
       </div>
       <StatusBar activeConnection={workspace.activeConnection} />
       <DialogsHost />
